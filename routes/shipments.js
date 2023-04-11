a7cb37015ff4d1c0c7c7aaa0321cd3464a4dc933
Violet Kester <kestervj89@gmail.com>
2023-04-11 19:51:54 -0400

    wrote tests for shipments route

--- routes/shipments.js
+++ routes/shipments.js
@@ -1,7 +1,10 @@
 "use strict";
 
-const express = require("express");
 const { BadRequestError } = require("../expressError");
+const jsonschema = require("jsonschema");
+const orderschema = require("../schemas/orderschema.json");
+const express = require("express");
+
 const router = new express.Router();
 
 const { shipProduct } = require("../shipItApi");
@@ -15,9 +18,17 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
-  if (req.body === undefined) {
-    throw new BadRequestError();
+
+  const result = jsonschema.validate(
+    req.body, orderschema, { required: true });
+    // TODO: change input to match doc string - currently missing "order:"
+  if (!result.valid) {
+    // pass validation errors to error handler
+    //  (the "stack" key is generally the most useful)
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
   }
+
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -5,21 +5,62 @@ const app = require("../app");
 
 
 describe("POST /", function () {
-  test("valid", async function () {
-    const resp = await request(app).post("/shipments").send({
-      productId: 1000,
-      name: "Test Tester",
-      addr: "100 Test St",
-      zip: "12345-6789",
-    });
 
+  // test("valid", async function () {
+  //   const resp = await request(app).post("/shipments").send({
+  //     productId: 1000,
+  //     name: "Test Tester",
+  //     addr: "100 Test St",
+  //     zip: "12345-6789",
+  //   });
+
+  //   expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  // });
+
+  // VALID DATA TEST
+  test("data is valid", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send({
+        "productId": 1001,
+        "name": "testUser",
+        "addr": "123 Main St.",
+        "zip": "30907"
+      });
+    // expect(resp.body).toEqual({ ordered: true });
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
 
+  // INVALID DATA TEST
+  test("returns JSON error for invalid data", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send({
+        "productId": 99,
+        "name": "testUser",
+        "addr": 222,
+        "zip": "30907"
+      });
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual(
+      {
+        "error": {
+          "message": [
+            "instance.productId must be greater than or equal to 1000",
+            "instance.addr is not of a type(s) string"
+          ],
+          "status": 400
+        }
+      });
+  });
+
+  // EMPTY BODY TEST
   test("throws error if empty request body", async function () {
     const resp = await request(app)
       .post("/shipments")
       .send();
     expect(resp.statusCode).toEqual(400);
   });
+
+
 });
